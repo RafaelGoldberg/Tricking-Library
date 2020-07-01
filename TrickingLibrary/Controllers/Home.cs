da7d0f1e31eddcc392a43dfdc086637108785348
Rafi Goldberg <rafaelgoldberg01@gmail.com>
2020-07-01 15:15:16 -0400

    Init

---
+++ TrickingLibrary/Controllers/Home.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrickingLibrary.Controllers
+{
+    [ApiController]
+    [Route("api/home")]
+    public class Home : ControllerBase
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Hello World";
+        }
+    }
+}
